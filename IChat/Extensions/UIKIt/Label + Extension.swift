dbc596492260724857aaaef08e2a0f76426246b1
Ksimolg <43907445+Ksimolg@users.noreply.github.com>
2021-11-29 00:07:06 +0300

    Add files via upload

---
+++ IChat/Extensions/UIKIt/Label + Extension.swift
@@ -0,0 +1,14 @@
+
+
+import UIKit
+
+extension UILabel {
+    
+    convenience init(text: String, font: UIFont? = .avenir20()) {
+        self.init()
+        
+        self.text = text
+        self.font = font
+    }
+}
+
